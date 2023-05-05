f159f8775619bc863a0862998e0b2962c19e896b
gaohan <869912332@qq.com>
2023-05-05 23:00:07 +0800

    JWT令牌

--- src/main/java/com/gaohan/case01/controller/LoginController.java
+++ src/main/java/com/gaohan/case01/controller/LoginController.java
@@ -3,11 +3,15 @@ package com.gaohan.case01.controller;
 import com.gaohan.case01.pojo.Emp;
 import com.gaohan.case01.pojo.Result;
 import com.gaohan.case01.service.EmpService;
+import com.gaohan.case01.utils.JwtUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @RestController
@@ -18,14 +22,28 @@ public class LoginController {
 
     /**
      * 登录
+     *
      * @param emp
      * @return
      */
     @PostMapping("/login")
-    public Result login(@RequestBody Emp emp){
+    public Result login(@RequestBody Emp emp) {
         log.info("员工登录：{}", emp);
         Emp e = empService.login(emp);
-        return e != null ? Result.success() : Result.error("用户名或密码错误");
+
+        // 登录成功，生成令牌，下发令牌
+        if (e != null) {
+            Map<String, Object> claims = new HashMap<>();
+            claims.put("id", e.getId());
+            claims.put("name", e.getName());
+            claims.put("username", e.getUsername());
+
+            String jwt = JwtUtils.generateJwt(claims);
+            return Result.success(jwt);
+        }
+
+        // 登录失败，返回错误信息
+        return Result.error("用户名或密码错误");
     }
 
 }
--- src/test/java/com/gaohan/case01/Case01ApplicationTests.java
+++ src/test/java/com/gaohan/case01/Case01ApplicationTests.java
@@ -1,7 +1,14 @@
 package com.gaohan.case01;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootTest
 class Case01ApplicationTests {
@@ -10,4 +17,34 @@ class Case01ApplicationTests {
     void contextLoads() {
     }
 
+    /**
+     * 生成JWT
+     */
+    @Test
+    public void testGenJwt() {
+        Map<String, Object> cliams = new HashMap<>();
+        cliams.put("id", 1);
+        cliams.put("name", "tom");
+
+        String jwt = Jwts.builder()
+                .signWith(SignatureAlgorithm.HS256, "gaohan") // 设置签名算法
+                .setClaims(cliams) // 设置自定义内容（载荷）
+                .setExpiration(new Date(System.currentTimeMillis() + 3600 * 1000)) // 设置有效期
+                .compact();
+
+        System.out.println(jwt);
+    }
+
+    /**
+     * 解析JWT
+     */
+    @Test
+    public void testParseJwt() {
+        Claims claims = Jwts.parser()
+                .setSigningKey("gaohan")
+                .parseClaimsJws("eyJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoidG9tIiwiaWQiOjEsImV4cCI6MTY4MzIxMDY3N30.ZQxuwZEnRXyTQEYYKEDc4Bl1pk6e_TvVFFtpS02A5GQ")
+                .getBody();
+
+        System.out.println(claims);
+    }
 }
